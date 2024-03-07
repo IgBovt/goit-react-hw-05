b018f18e7da3a1d374fce3879c0591b1cffea3cc
IgBovt <140014814+IgBovt@users.noreply.github.com>
2024-03-07 22:22:46 +0200

    made not found

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,3 +1,11 @@
+import css from './NotFoundPage.module.css';
+import { TfiFaceSad } from 'react-icons/tfi';
+
 export default function NotFoundPage() {
-  return <p>not found</p>;
+  return (
+    <div className={css.container}>
+      <p className={css.text}>Sorry... we haven`t found this page...</p>
+      <TfiFaceSad className={css.icon} />
+    </div>
+  );
 }
